9793e96ec655a92c3cd12831a9a3a6b75af1e26c
udeved <artoo@manjaro.org>
2016-08-19 11:08:22 +0200

    postcfg: add postcfg.conf and clean up code

--- src/modules/postcfg/main.py
+++ src/modules/postcfg/main.py
@@ -22,44 +22,77 @@ import libcalamares
 import os
 import shutil
 from subprocess import check_call
-from os.path import join
+from os.path import join, exists
 from libcalamares import target_env_call
+
+class ConfigController:
+    def __init__(self):
+        self.__root = libcalamares.globalstorage.value( "rootMountPoint" )
+        self.__keyrings = libcalamares.job.configuration.get('keyrings', [])
+
+    @property
+    def root(self):
+        return self.__root
+      
+    def init_keyring(self):
+        check_target_env_call(["pacman-key", "--init"])
+
+    def populate_keyring(self):
+        check_target_env_call(["pacman-key", "--populate"] + self.keyrings)
+        
+    def run(self):
+        self.init_keyring()
+        self.populate_keyring()
+        
+    def setExpression(self, pattern, file):
+        check_call(["sed", "-e", pattern, "-i", join(self.root, file)])
+        
+    def terminate(self, proc):
+        target_env_call(['killall', '-9', proc])
+        
+    def copy_file(self, file):
+        if exists("/" + file):
+            shutil.copy2("/" + file, join(self.root, file))
+    
+    def remove_pkg(self, pkg, path):
+        if exists(join(self.root, path)):
+            target_env_call(['pacman', '-R', '--noconfirm', pkg])
+    
+    def run(self):
+        self.init_keyring()
+        self.populate_keyring()
+
+        # Remove calamares
+        self.remove_pkg("calamares", "usr/bin/calamares")
+
+        # Copy mirror list
+        self.copy_file('etc/pacman.d/mirrorlist')
+        
+        # Workaround for pacman-key bug FS#45351 https://bugs.archlinux.org/task/45351
+        # We have to kill gpg-agent because if it stays around we can't reliably unmount
+        # the target partition.
+        self.terminate('gpg-agent')
+        
+        # configure openrc specific settings
+        if exists(join(self.root, "usr/bin/openrc")):
+            self.setExpression('s|^.*rc_shell=.*|rc_shell="/usr/bin/sulogin"|', "etc/rc.conf")
+            self.setExpression('s|^.*rc_controller_cgroups=.*|rc_controller_cgroups="YES"|', "etc/rc.conf")
+            exp = 's|^.*keymap=.*|keymap="' + libcalamares.globalstorage.value("keyboardLayout") + '"|'
+            self.setExpression(exp, "etc/conf.d/keymaps")
+            for dm in libcalamares.globalstorage.value("displayManagers"):
+                exp = 's|^.*DISPLAYMANAGER=.*|DISPLAYMANAGER="' + dm + '"|'
+                self.setExpression(exp, "etc/conf.d/xdm")
+                if dm == "lightdm":
+                    self.setExpression('s|^.*minimum-vt=.*|minimum-vt=7|', "etc/lightdm/lightdm.conf")
+                    self.setExpression('s|pam_systemd.so|pam_ck_connector.so nox11|', "etc/pam.d/lightdm-greeter")
+        
+        # Update grub.cfg
+        if exists(join(self.root, "usr/bin/update-grub")) and libcalamares.globalstorage.value("bootLoader") is not None:
+            target_env_call(["update-grub"])
     
 def run():
     """ Misc postinstall configurations """
 
-    install_path = libcalamares.globalstorage.value( "rootMountPoint" )
-
-    # Update grub.cfg
-    if os.path.exists(join(install_path, "usr/bin/update-grub")) and libcalamares.globalstorage.value("bootLoader") is not None:
-        target_env_call(["update-grub"])
-
-    # Remove calamares
-    if os.path.exists(join(install_path, "usr/bin/calamares")):
-        target_env_call(['pacman', '-R', '--noconfirm', 'calamares'])
-
-    # Copy mirror list
-    shutil.copy2('/etc/pacman.d/mirrorlist',
-             join(install_path, 'etc/pacman.d/mirrorlist'))
-
-    target_env_call(['pacman-key', '--init'])
-    target_env_call(['pacman-key', '--populate', 'archlinux', 'manjaro'])
+    config = ConfigController()
     
-    # Workaround for pacman-key bug FS#45351 https://bugs.archlinux.org/task/45351
-    # We have to kill gpg-agent because if it stays around we can't reliably unmount
-    # the target partition.
-    target_env_call(['killall', '-9', 'gpg-agent'])
-        
-    if os.path.exists(join(install_path, "usr/bin/openrc")):
-        check_call(["sed", "-e", 's|^.*rc_shell=.*|rc_shell="/usr/bin/sulogin"|', "-e", 's|^.*rc_controller_cgroups=.*|rc_controller_cgroups="YES"|', "-i", join(install_path, "etc/rc.conf")])
-        kbl = libcalamares.globalstorage.value("keyboardLayout")
-        rep_str = 's|^.*keymap=.*|keymap="' + kbl + '"|'
-        check_call(["sed", "-e", rep_str, "-i", join(install_path, "etc/conf.d/keymaps")])
-        for dm in libcalamares.globalstorage.value("displayManagers"):
-            rep_str = 's|^.*DISPLAYMANAGER=.*|DISPLAYMANAGER="' + dm + '"|'
-            check_call(["sed", "-e", rep_str, "-i", join(install_path, "etc/conf.d/xdm")])
-            if dm == "lightdm":
-                check_call(["sed", "-e", 's|^.*minimum-vt=.*|minimum-vt=7|', "-i", join(install_path, "etc/lightdm/lightdm.conf")])
-                check_call(["sed", "-e", 's|pam_systemd.so|pam_ck_connector.so nox11|', "-i", join(install_path, "etc/pam.d/lightdm-greeter")])
-
-    return None
+    return config.run()
