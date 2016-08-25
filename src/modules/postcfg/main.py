32f794f13dfde5af89056d0f50886eac8d94be63
Philip <philm@manjaro.org>
2016-08-25 09:38:59 +0200

    [modification] add changes for CAL v2.4

--- src/modules/postcfg/main.py
+++ src/modules/postcfg/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+#
 # === This file is part of Calamares - <http://github.com/calamares> ===
 #
 #   Copyright 2014 - 2015, Philip Müller <philm@manjaro.org>
+#   Copyright 2016, Artoo <artoo@manjaro.org>
 #
 #   Calamares is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -22,51 +24,63 @@ import libcalamares
 import os
 import shutil
 
+from os.path import join, exists
+from libcalamares.utils import target_env_call, check_target_env_call
+
+class ConfigController:
+    def __init__(self):
+        self.__root = libcalamares.globalstorage.value( "rootMountPoint" )
+        self.__keyrings = libcalamares.job.configuration.get('keyrings', [])
+        
+    @property
+    def root(self):
+        return self.__root
+    
+    @property
+    def keyrings(self):
+        return self.__keyrings
+      
+    def init_keyring(self):
+        check_target_env_call(["pacman-key", "--init"])
+
+    def populate_keyring(self):
+        check_target_env_call(["pacman-key", "--populate"] + self.keyrings)
+        
+    def terminate(self, proc):
+        target_env_call(['killall', '-9', proc])
+        
+    def copy_file(self, file):
+        if exists("/" + file):
+            shutil.copy2("/" + file, join(self.root, file))
+    
+    def remove_pkg(self, pkg, path):
+        if exists(join(self.root, path)):
+            target_env_call(['pacman', '-R', '--noconfirm', pkg])
+    
+    def run(self):
+        self.init_keyring()
+        self.populate_keyring()
+
+        # Remove calamares
+        self.remove_pkg("calamares", "usr/bin/calamares")
+
+        # Copy mirror list
+        self.copy_file('etc/pacman.d/mirrorlist')
+        
+        # Workaround for pacman-key bug FS#45351 https://bugs.archlinux.org/task/45351
+        # We have to kill gpg-agent because if it stays around we can't reliably unmount
+        # the target partition.
+        self.terminate('gpg-agent')
+        
+        # Update grub.cfg
+        if exists(join(self.root, "usr/bin/update-grub")) and libcalamares.globalstorage.value("bootLoader") is not None:
+            target_env_call(["update-grub"])
+        
+        return None
+    
 def run():
     """ Misc postinstall configurations """
 
-    install_path = libcalamares.globalstorage.value( "rootMountPoint" )
-
-    # Update grub.cfg
-    if os.path.exists("{!s}/usr/bin/update-grub".format(install_path)) and libcalamares.globalstorage.value("bootLoader") is not None:
-        libcalamares.utils.target_env_call(["update-grub"])
-
-    # Remove calamares
-    if os.path.exists("{!s}/usr/bin/calamares".format(install_path)):
-        libcalamares.utils.target_env_call(['pacman', '-R', '--noconfirm', 'calamares'])
-
-    # Copy mirror list
-    shutil.copy2('/etc/pacman.d/mirrorlist',
-             os.path.join(install_path, 'etc/pacman.d/mirrorlist'))
-
-    # Copy random generated keys by pacman-init to target
-    if os.path.exists("{!s}/etc/pacman.d/gnupg".format(install_path)):
-        os.system("rm -rf {!s}/etc/pacman.d/gnupg".format(install_path))
-    os.system("cp -a /etc/pacman.d/gnupg {!s}/etc/pacman.d/".format(install_path))
-    libcalamares.utils.target_env_call(['pacman-key', '--populate', 'archlinux', 'manjaro'])
+    config = ConfigController()
     
-    # Workaround for pacman-key bug FS#45351 https://bugs.archlinux.org/task/45351
-    # We have to kill gpg-agent because if it stays around we can't reliably unmount
-    # the target partition.
-    libcalamares.utils.target_env_call(['killall', '-9', 'gpg-agent'])
-
-    # Set /etc/keyboard.conf (keyboardctl is depreciated)
-    if os.path.exists("{!s}/etc/keyboard.conf".format(install_path)):
-        keyboard_layout = libcalamares.globalstorage.value("keyboardLayout")
-        keyboard_variant = libcalamares.globalstorage.value("keyboardVariant")
-        consolefh = open("{!s}/etc/keyboard.conf".format(install_path), "r")
-        newconsolefh = open("{!s}/etc/keyboard.new".format(install_path), "w")
-        for line in consolefh:
-            line = line.rstrip("\r\n")
-            if(line.startswith("XKBLAYOUT=")):
-                newconsolefh.write("XKBLAYOUT=\"{!s}\"\n".format(keyboard_layout))
-            elif(line.startswith("XKBVARIANT=") and keyboard_variant != ''):
-                newconsolefh.write("XKBVARIANT=\"{!s}\"\n".format(keyboard_variant))
-            else:
-                newconsolefh.write("{!s}\n".format(line))
-        consolefh.close()
-        newconsolefh.close()
-        libcalamares.utils.target_env_call(['mv', '/etc/keyboard.conf', '/etc/keyboard.conf.old'])
-        libcalamares.utils.target_env_call(['mv', '/etc/keyboard.new', '/etc/keyboard.conf'])
-
-    return None
+    return config.run()
